fc745901fc1ea57cf93cd0b2f2842c4e344ced8b
Francisco Flores <54539494+franciscofl12@users.noreply.github.com>
2022-03-02 11:30:12 +0100

    Nav changed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,7 +25,7 @@
         if(auth()->user()->avatar == null) {
         ?>
         <img class="mx-auto rounded-full border-2 border-indigo-400 mb-4"
-        s     src="{{ asset('../resources/img/defaultavatar.jpg')}}" width="150px">
+             s src="{{ asset('../resources/img/defaultavatar.jpg')}}" width="150px">
         <?php
         } else {
         ?>
@@ -33,27 +33,34 @@
         <?php
         }
         ?>
-        <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="text-white hover:bg-gray-800 font-medium rounded-lg text-sm px-4 mb-4 py-2.5 text-center inline-flex items-center" type="button">{{auth()->user()->username}} <svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button><br>
+        <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider"
+                class="text-white hover:bg-gray-800 font-medium rounded-lg text-sm px-4 mb-4 py-2.5 text-center inline-flex items-center"
+                type="button">{{auth()->user()->username}}
+            <svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                 xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+            </svg>
+        </button>
+        <br>
 
         <!-- Dropdown menu -->
-        <div id="dropdownDivider" class="hidden z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100 shadow">
+        <div id="dropdownDivider"
+             class="hidden z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100 shadow">
             <div class="py-1">
-                <a href="{{route('profile')}}" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Edit Profile</a>
+                <a href="{{route('profile')}}" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Edit
+                    Profile</a>
             </div>
             <div class="py-1">
                 <a href="{{route('logout')}}" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
             </div>
         </div>
 
-        <button class="bg-indigo-400 hover:bg-white hover:text-gray-900 transition duration-200 mb-4 text-white font-bold py-2 px-4 rounded"
-                type="button" data-modal-toggle="defaultModal">
-            Edit profile
-        </button>
         <?php
         if (auth()->user()->rol == "admin") {
         ?>
         <a href="{{route('admin')}}">
-            <button class="bg-orange-400 hover:bg-white transition duration-200 hover:text-gray-900 text-gray-900 font-bold py-2 px-4 rounded">
+            <button
+                class="bg-orange-400 hover:bg-white transition duration-200 hover:text-gray-900 text-gray-900 font-bold py-2 px-4 rounded">
                 Admin Panel
             </button>
         </a>
@@ -65,27 +72,14 @@
     <!-- nav -->
     <nav>
         <a href="#" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-400 hover:text-white">
-            Home
-        </a>
-        <a href="" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-400 hover:text-white">
-            About
-        </a>
-        <a href="" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-400 hover:text-white">
-            Features
+            Shared Archives
         </a>
         <a href="" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-400 hover:text-white">
             Pricing
         </a>
     </nav>
-    <div class="flex w-full items-center justify-center bg-grey-lighter">
-        <label
-            class="w-64 flex flex-col border-indigo-400  items-center px-4 py-6 bg-white text-indigo-400 transition duration-700 rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-indigo-400 hover:text-white">
-            <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path
-                    d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"/>
-            </svg>
-            <span class="mt-2 text-base leading-normal">Upload a File</span>
-            <input type='file' class="hidden"/>
-        </label>
+    <div class="text-indigo-400 text-center p-4" style="background-color: rgba(0, 0, 0, 0.3);">
+        © 2022 Copyright: xDrive , made by
+        <a class="text-gray-200" href="https://franciscofl.dev/">franciscofl12</a>
     </div>
 </div>
